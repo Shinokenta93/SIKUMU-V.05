d3aa45866c3b1402a6dc2fd698ce66559275b5a1
Shinokenta93 <86036714+Shinokenta93@users.noreply.github.com>
2021-06-17 13:40:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SIKUMU</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo-ahu.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/poster.css">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <style type="text/css">
+   body { background: #040F19 !important; } /* Adding !important forces the browser to overwrite the default style applied by Bootstrap */
+  </style>
+
+
+  <!-- =======================================================
+  * Template Name: Knight - v2.2.1
+  * Template URL: https://bootstrapmade.com/knight-free-bootstrap-theme/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</style>
+</head>
+
+<body>
+<header id="header" class="d-flex align-items-center">
+    <div class="container" #0f0c42>
+
+      <!-- The main logo is shown in mobile version only. The centered nav-logo in nav menu is displayed in desktop view  -->
+      <div class="logo d-block d-lg-none">
+        <a href="index.html"><img src="assets/img/logo-ahu.png" alt="" class="img-fluid"></a>
+		
+		
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        
+
+          <li class="nav-logo"><a href="index.html"><img src="assets/img/logo-ahu.png" alt="" class="img-fluid"></a></li>
+
+         
+      </nav><!-- .nav-menu -->
+	<div><h3 style="color:white;">Direktorat Jenderal Administrasi Hukum Umum</h3></div>
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <!-- <marquee behavior="alternate" direction="right"><h1>SELAMAT DATANG DI SISTEM APLIKASI KUNJUNGAN TAMU</h1></marquee> -->
+   	<div class="container-fluid">
+      		<img src="assets/img/katakata.png" class="rounded mx-auto d-block" width='80%'>
+  	</div>
+  </section>
+  <div class="row">
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+      <a class="button" href="halaman/daftar/index.php" role="button" target="_blank">Daftar Antrian</a>
+    </div>
+    <div class="col-sm">
+       <a class="button" href="https://portal.ahu.go.id/site/struktur_organisasi" role="button" target="_blank">Struktur Organisasi</a>
+    </div>
+    <div class="col-sm">
+	<!-- btn btn-primary btn-lg -->
+     <a class="button" href="halaman/cari/index.php" role="button" target="_blank">Cari Tiket</a>
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
+
+  <!-- End Hero -->
+
+  <!-- ======= Header ======= -->
+  
+
+  <main id="hero">
+  
+  
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; <strong><span>Direktorat Jenderal Administrasi Hukum Umum 2021 | Kementerian Hukum Dan Hak Asasi Manusia Republik Indonesia</span></strong><br/>
+		<strong><span>Email : cs@ahu.go.id | Fax : 1500105</span></strong>
+
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/knight-free-bootstrap-theme/ -->       
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
